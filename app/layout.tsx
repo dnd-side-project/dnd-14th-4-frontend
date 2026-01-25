ab6376943826e50f2503a4a2036127774fc8aa42
betteree <kkne1017@gmail.com>
2026-01-25 22:51:26 +0900

    refactor: 하드코딩 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="w-full max-w-[430px] min-h-screen bg-white shadow-xl">
+        <div className="w-full max-w-mobile min-h-screen bg-white shadow-xl">
           {children}
         </div>
       </body>
