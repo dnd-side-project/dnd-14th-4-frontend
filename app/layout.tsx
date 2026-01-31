877ca87098666bc5e01bcefa4f58cb1d2a28ab52
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2026-01-31 16:17:15 +0900

    design: 디자인시스템 컬러 추가, 버튼 공통 컴포넌트 구현

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="w-full max-w-mobile min-h-screen bg-white shadow-xl">
+        <div className="w-full max-w-mobile min-h-screen bg-white shadow-xl px-5">
           {children}
         </div>
       </body>
