89ad57b764848b492b1230f491d466976e4b502b
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2026-02-21 14:22:18 +0900

    refactor: splash 초기 페이지 구현,  코드리뷰 반영- 훅 수정

--- src/views/splash/model/constants.ts
+++ src/views/splash/model/constants.ts
@@ -1,2 +1,2 @@
 /** 스플래시 노출 시간(ms). 이후 자동으로 다음 화면으로 이동 */
-export const SPLASH_DURATION_MS = 2000;
+export const SPLASH_DURATION_MS = 20000;
--- src/views/splash/ui/SplashPage.tsx
+++ src/views/splash/ui/SplashPage.tsx
@@ -3,30 +3,24 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { IcSvgLogo } from "@/shared/icons";
-import { useAuthStore } from "@/shared/store/authStore";
 import { SPLASH_DURATION_MS } from "../model/constants";
 
 export function SplashPage() {
   const router = useRouter();
-  const isAuthenticated = useAuthStore((s) => s.isAuthenticated);
-
+  //TODO: 로그인 여부 확인 후 이동
   useEffect(() => {
     const t = setTimeout(() => {
-      if (isAuthenticated) {
-        router.replace("/");
-      } else {
-        router.replace("/login");
-      }
+      router.replace("/");
     }, SPLASH_DURATION_MS);
+  
     return () => clearTimeout(t);
-  }, [router, isAuthenticated]);
+  }, [router]);
 
   return (
-    <div className="fixed inset-0 z-[100] flex min-h-dvh flex-col items-center justify-center bg-white">
-      <IcSvgLogo width={120} height={120} className="shrink-0" />
-      <p className="mt-4 type-heading2 text-label-default">
-        What&apos;s in my pack
-      </p>
+    <div
+      className="fixed inset-0 z-[100] flex items-center justify-center bg-cover bg-center bg-no-repeat"
+    >
+      <IcSvgLogo className="w-30 h-30" />
     </div>
   );
 }
