ab9382d9973b8a6ffac1bde4ff7bbc27a2d51c02
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2026-02-26 20:56:51 +0900

    feat: splash, login 페이지 수정사항 반영

--- src/app/intro/page.tsx
+++
@@ -1,5 +0,0 @@
-import { IntroPage } from "@/views/intro/IntroPage";
-
-export default function Page() {
-  return <IntroPage />;
-}
--- src/shared/icons/ic_doneround.tsx
+++ src/shared/icons/ic_doneround.tsx
@@ -7,11 +7,11 @@ const IcSvgDoneRound = (props: SVGProps<SVGSVGElement>) => (
     {...props}
   >
     <path
-      fill="currentColor"
+      fill="#111"
       d="M0 12C0 5.373 5.373 0 12 0s12 5.373 12 12-5.373 12-12 12S0 18.627 0 12"
     />
     <path
-      stroke="#fff"
+      stroke="currentColor"
       strokeLinecap="round"
       strokeWidth={2}
       d="m5 14 3.233 2.425a1 1 0 0 0 1.374-.167L18 6"
