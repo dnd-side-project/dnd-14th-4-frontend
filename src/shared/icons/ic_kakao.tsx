8e18eb247d7074724bf7494d6bb8d58746eabf48
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2026-02-26 21:56:16 +0900

    fix: ic 컴포넌트 롤백

---
+++ src/shared/icons/ic_kakao.tsx
@@ -0,0 +1,15 @@
+import type { SVGProps } from "react";
+const IcSvgKakao = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 18 17"
+    {...props}
+  >
+    <path
+      fill="#181600"
+      d="M9 0c4.972 0 9 3.178 9 7.097s-4.028 7.097-9 7.097q-.821 0-1.6-.113c-.519.366-3.519 2.472-3.803 2.513 0 0-.116.043-.216-.013s-.081-.21-.081-.21c.031-.205.781-2.787.919-3.265C1.684 11.853 0 9.628 0 7.094 0 3.175 4.028 0 9 0"
+    />
+  </svg>
+);
+export default IcSvgKakao;
