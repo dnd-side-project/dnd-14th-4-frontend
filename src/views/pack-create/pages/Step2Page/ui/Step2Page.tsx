6835d47b5c53bdde025493887a38c9cab053b031
HYERYEON <100520490+anhyeryeon2@users.noreply.github.com>
2026-02-28 00:27:45 +0900

    Design/#46 (#73)
    * refactor: src/app 폴더구조 router group 적용
    
    * feat: pack 생성 퍼블리싱, api연결
    
    * fix: 빌드에러 수정
    
    * feat: 팩 생성 선택로직 추가
    
    * feat: 팩 생성 api 변경

--- src/views/pack-create/pages/Step2Page/ui/Step2Page.tsx
+++ src/views/pack-create/pages/Step2Page/ui/Step2Page.tsx
@@ -46,7 +46,7 @@ export default function Step2Page() {
       <FlowLayout.Footer
         label="다음"
         disabled={!canNext}
-        onClick={() => router.push("/pack/create/step-3")}
+        onClick={() => router.push("/pack-create/step-3")}
       />
     </FlowLayout>
   );
--- src/views/pack-create/pages/Step3Page/ui/Step3Page.tsx
+++ src/views/pack-create/pages/Step3Page/ui/Step3Page.tsx
@@ -44,7 +44,7 @@ export default function Step3Page() {
       <FlowLayout.Footer
         label="다음"
         disabled={selectedCategory.length === 0}
-        onClick={() => router.push("/pack/create/step-4")}
+        onClick={() => router.push("/pack-create/step-4")}
       />
     </FlowLayout>
   );
